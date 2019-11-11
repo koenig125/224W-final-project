07e25d8fe80208a229c298681100b836c084d61b
Christopher Koenig <koenig97@stanford.edu>
2019-11-11 13:18:34 -0800

    Split edge distribution and community detection into separate scripts.

--- community_detection.py
+++ community_detection.py
@@ -1,56 +1,14 @@
 """
-Runs Louvain community detection algorithm on the Leeds Butterfly dataset
-and provides species distribution analysis for detected communities. Also
-provides classification results based on majority-label predictions from
-the communities identified by Louvain.
+Script to run Louvain community detection algorithm on the BIOSNAP Leeds Butterfly 
+Dataset with Network Enhancement. Provides classification results (accuracy and 
+confusion matrix) based on majority-label predictions from the communities 
+identified by Louvain. Also plots communities identified by the algorithm.
 """
 
-import numpy as np
 import sklearn.metrics as metrics
 from community import best_partition as louvain
 
 import utils
-
-
-def get_edge_distribution(graph):
-    """
-    Calculate the distribution of edge weights between nodes of different classes.
-
-    :param - graph: nx.Graph object representing butterfly similarity network
-    return: Row-normalized 2D numpy array for which each row and column represents 
-    a species. Entry (i, j) is the percentage of the total edge weight for nodes  
-    from species i that derives from edges to nodes from species j.
-    """
-    all_labels = set([graph.nodes[n]['label'] for n in graph.nodes])
-    num_classes = len(all_labels)
-    edge_weights = np.zeros((num_classes, num_classes))
-    for n in graph.nodes:
-        l1 = graph.nodes[n]['label']
-        for neighbor in graph.adj[n]:
-            l2 = graph.nodes[neighbor]['label']
-            weight = graph.adj[n][neighbor]['weight']
-            edge_weights[l1 - 1][l2 - 1] += weight
-    row_sums = edge_weights.sum(axis=1)
-    normalized_distribution = edge_weights / row_sums[:, np.newaxis]
-    return normalized_distribution
-
-
-def get_labels(graph, nodes):
-    """
-    Get the labels of each node and group nodes by labels.
-
-    :param - graph: nx.Graph object representing butterfly similarity network
-    :param - nodes: list of node ids from graph
-    return: dictionary mapping labels to nodes
-    """
-    labels = {}
-    for node in nodes:
-        l = graph.nodes[node]['label']
-        if l in labels:
-            labels[l].append(node)
-        else:
-            labels[l] = [node]
-    return labels
 
 
 def partition(graph):
@@ -89,6 +47,24 @@ def predict(graph, communities):
     return preds
 
 
+def get_labels(graph, nodes):
+    """
+    Get the labels of each node and group nodes by labels.
+
+    :param - graph: nx.Graph object representing butterfly similarity network
+    :param - nodes: list of node ids from graph
+    return: dictionary mapping labels to nodes
+    """
+    labels = {}
+    for node in nodes:
+        l = graph.nodes[node]['label']
+        if l in labels:
+            labels[l].append(node)
+        else:
+            labels[l] = [node]
+    return labels
+
+
 def report_classification_results(predictions, labels):
     """
     Calculate classification accuracy and confusion matrix given predictions and labels.
@@ -99,21 +75,18 @@ def report_classification_results(predictions, labels):
     accuracy = metrics.accuracy_score(labels, predictions)
     print('Majority Label Classification Accuracy: {0:.5f}'.format(accuracy))
     confusion_matrix = metrics.confusion_matrix(labels, predictions)
-    utils.plot_heatmap(confusion_matrix, 'Confusion Matrix - Majority Label Predictions from Louvain Communities', 'images/confusion_matrix.png', 'Predicted label', 'True label')
+    utils.plot_heatmap(confusion_matrix, 'Confusion Matrix - Majority Label Predictions from Louvain Communities', 'images/cm_louvain.png', 'Predicted label', 'True label')
 
 
 def main():
     graph = utils.load_graph()
     position = utils.get_positions(graph)
-    
-    edge_distribution = get_edge_distribution(graph)
-    utils.plot_heatmap(edge_distribution, 'Row-Normalized Inter-Species Edge Weight Distribution Post Network Enhancement', 'images/edge_distribution.png')
 
     true_communities = get_labels(graph, list(graph.nodes))
-    utils.plot_communities(graph, position, true_communities, labels=True, title='Butterfly Similarity Network - True Communities', path='images/true_communities.png')
+    utils.plot_communities(graph, position, true_communities, labels=True, title='Butterfly Similarity Network - True Communities', path='images/communities_true.png')
 
     communities = partition(graph)
-    utils.plot_communities(graph, position, communities, labels=False, title='Butterfly Similarity Network - Louvain Communities', path='images/louvain_communities.png')
+    utils.plot_communities(graph, position, communities, labels=False, title='Butterfly Similarity Network - Louvain Communities', path='images/communities_louvain.png')
 
     graph_nodes = sorted(list(graph.nodes))
     predictions = predict(graph, communities)
---
+++ edge_distribution.py
@@ -0,0 +1,42 @@
+"""
+Script to calculate inter-species edge weight distribution for the BIOSNAP Leeds
+Butterfly Dataset. Edge weights derived from Network Enhancement processing
+(see https://snap.stanford.edu/biodata/datasets/10029/10029-SS-Butterfly.html).
+"""
+
+import numpy as np
+
+import utils
+
+
+def get_edge_distribution(graph):
+    """
+    Calculate the distribution of edge weights between nodes of different classes.
+
+    :param - graph: nx.Graph object representing butterfly similarity network
+    return: Row-normalized 2D numpy array for which each row and column represents 
+    a species. Entry (i, j) is the percentage of the total edge weight for nodes  
+    from species i that derives from edges to nodes from species j.
+    """
+    all_labels = set([graph.nodes[n]['label'] for n in graph.nodes])
+    num_classes = len(all_labels)
+    edge_weights = np.zeros((num_classes, num_classes))
+    for n in graph.nodes:
+        l1 = graph.nodes[n]['label']
+        for neighbor in graph.adj[n]:
+            l2 = graph.nodes[neighbor]['label']
+            weight = graph.adj[n][neighbor]['weight']
+            edge_weights[l1 - 1][l2 - 1] += weight
+    row_sums = edge_weights.sum(axis=1)
+    normalized_distribution = edge_weights / row_sums[:, np.newaxis]
+    return normalized_distribution
+
+
+def main():
+    graph = utils.load_graph()
+    edge_distribution = get_edge_distribution(graph)
+    utils.plot_heatmap(edge_distribution, 'Row-Normalized Inter-Species Edge Weight Distribution Post Network Enhancement', 'images/edge_distribution.png')
+
+
+if __name__=='__main__':
+    main()
