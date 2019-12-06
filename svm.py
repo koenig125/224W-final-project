9d2aa4cf30d9f99e0172ce3017e11034b7616c32
Christopher Koenig <koenig97@stanford.edu>
2019-12-05 17:07:41 -0800

    Change method for generating random ordering of data.

--- svm.py
+++ svm.py
@@ -7,6 +7,7 @@ import argparse
 
 import numpy as np
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
 import utils
@@ -22,6 +23,40 @@ def parse_args():
                         help='Name of input file for node embeddings')
 
     return parser.parse_args()
+
+
+def load_splits(embeddings_path):
+    """
+    Generate train/test splits for the butterfly embeddings. Embeddings 
+    need to be generated prior by running node2vec as specified here: 
+    https://github.com/koenig125/224W-final-project.
+
+    return: 4-tuple holding the training embeddings, training labels, testing
+    embeddings, and testing labels for the BIOSNAP butterfly similarity network.
+    """
+    labels = utils.load_labels()
+    embeddings = utils.load_embeddings(embeddings_path)
+    num_nodes = len(labels)
+
+    nids = utils.shuffle_ids(list(range(num_nodes)))
+    X = np.array([embeddings[n] for n in nids])
+    y = np.array([labels[n] for n in nids])
+
+    test = int(num_nodes * .8)
+    X_train, y_train = X[:test], y[:test]
+    X_test, y_test = X[test:], y[test:]
+    return X_train, X_test, y_train, y_test
+
+
+def standardize_data(X_train, X_test):
+    """
+    Standardizes X_train to zero mean and unit variance, then applies the 
+    transformation that was executed on X_train to X_test.
+    """
+    scaler = StandardScaler().fit(X_train)
+    X_train_transformed = scaler.transform(X_train)
+    X_test_transformed = scaler.transform(X_test)
+    return X_train_transformed, X_test_transformed
 
 
 def hyperparameter_search(svc, X, y):
@@ -42,8 +77,8 @@ def hyperparameter_search(svc, X, y):
 def main(args):
     # Load and standardize data.
     embedding_file_path = 'node2vec/embeddings/' + args.input
-    X_train, X_test, y_train, y_test = utils.load_splits(embedding_file_path)
-    X_train, X_test = utils.standardize_data(X_train, X_test)
+    X_train, X_test, y_train, y_test = load_splits(embedding_file_path)
+    X_train, X_test = standardize_data(X_train, X_test)
 
     # Train classifier and make predictions.
     optimal_svc = hyperparameter_search(SVC(), X_train, y_train)
--- utils.py
+++ utils.py
@@ -51,30 +51,16 @@ def load_graph():
     return G
 
 
-def load_splits(embeddings_path):
+def shuffle_ids(node_ids):
     """
-    Generate train/test splits for butterfly data embeddings. The embeddings 
-    need to be generated prior by running node2vec as specified here: 
-    https://github.com/koenig125/224W-final-project.
+    Generate a random permutation of node ids.
 
-    return: 4-tuple holding the training embeddings, training labels, testing
-    embeddings, and testing labels for the BIOSNAP butterfly similarity network.
+    return: list of node ids in randomly permuted order
     """
-    labels = load_labels()
-    embeddings = load_embeddings(embeddings_path)
-    return sklearn.model_selection.train_test_split(
-        embeddings, labels, test_size=0.4, random_state=224)
-
-
-def standardize_data(X_train, X_test):
-    """
-    Standardizes X_train to zero mean and unit variance, then applies the 
-    transformation that was executed on X_train to X_test.
-    """
-    scaler = sklearn.preprocessing.StandardScaler().fit(X_train)
-    X_train_transformed = scaler.transform(X_train)
-    X_test_transformed = scaler.transform(X_test)
-    return X_train_transformed, X_test_transformed
+    np.random.seed(0)
+    nids = node_ids.copy()
+    np.random.shuffle(nids)
+    return nids
 
 
 def load_embeddings(path):
