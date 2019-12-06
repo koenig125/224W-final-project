95880f9f0ffff6ea9eb8652e0f1d79499d89a4b9
Christopher Koenig <koenig97@stanford.edu>
2019-12-06 11:29:37 -0800

    Add flexibility for plotting validation accuracies.

--- gnn/plot.py
+++ gnn/plot.py
@@ -1,29 +1,60 @@
+import os
+import sys
+
 import numpy as np
+from matplotlib import cm
 from matplotlib import pyplot as plt
 
-import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import utils
 
+validation_dir = 'gnn/validation/'
 
-def main():
-    vacc_GCN = np.load('gnn/validation/GCN.npy')
-    vacc_SAGE = np.load('gnn/validation/GraphSage.npy')
-    vacc_GAT = np.load('gnn/validation/GAT.npy')
-    epochs = len(vacc_GCN)
+
+def plot_validation_accuracies(filenames, plot_name):
+    validation_accuracies = []
+    for f in filenames:
+        v = np.load(validation_dir + f)
+        validation_accuracies.append(v)
+    
+    epochs = [len(v) for v in validation_accuracies]
+    assert (len(set(epochs)) == 1), 'Epochs not consistent'
+    num_epochs = epochs[0]
 
     plt.clf()
-    plt.plot(list(range(1, epochs + 1)), vacc_GCN, color = 'r', label = 'GCN')
-    plt.plot(list(range(1, epochs + 1)), vacc_SAGE, linestyle = 'dashed', color = 'g', label = 'SAGE')
-    plt.plot(list(range(1, epochs + 1)), vacc_GAT, linestyle = 'dotted', color = 'b', label = 'GAT')
+    for i, f in enumerate(filenames):
+        y = validation_accuracies[i]
+        x = list(range(1, num_epochs + 1))
+        c_index = int((i + 1) / len(filenames) * 255)
+        c = cm.get_cmap('jet')(c_index)[:3]
+        plt.plot(x, y, color=c, label=f)
+        print('Best accuracy for', f, '-', max(y))
     
     plt.title('Validation Accuracy vs. Epochs for Node Classification')
-    plt.xlabel('Epoch')
     plt.ylabel('Validation Accuracy')
+    plt.xlabel('Epoch')
     plt.legend()
     utils.make_dir('images/gnn')
-    plt.savefig('images/gnn/node-classification-training.png')
+    plt.savefig('images/gnn/' + plot_name)
 
 
-if __name__ == '__main__':
-	main()
+def main():
+    model_types = ['GCN', 'GraphSage', 'GAT']
+    epochs = ['400']
+    num_layers = ['2', '3', '4']
+    hidden_dim = ['32', '64', '128']
+    dropout = ['0.0', '0.1']
+
+    for m in model_types:
+        for e in epochs:
+            for n in num_layers:
+                filenames = []
+                for h in hidden_dim:
+                    for d in dropout:
+                        f = '_'.join([m, e, n, h, d]) + '.npy'
+                        filenames.append(f)
+                plot_validation_accuracies(filenames, 'm=' + m + '_n=' + n + '.png')
+
+
+if __name__ == "__main__":
+    main()
